d24345fb243c620dfed4424b5a5eb9eb4966b851
mrseanmcrawford <Mrseanmcrawford@icloud.com>
2021-01-05 22:07:00 -0800

    Update officeHours.py

--- officeHours.py
+++ officeHours.py
@@ -1,7 +1,7 @@
 title = "Office Hour"
 print(title)
 
-message = "Office hour are from 9:00 am to 9:00 pm."
+message = "Office hours are from 9:00 am to 9:00 pm."
 print(message)
 
 portlandTime = 8
@@ -95,3 +95,5 @@ def londonBranch():
 portlandBranch()
 newYorkBranch()
 londonBranch()
+
+
