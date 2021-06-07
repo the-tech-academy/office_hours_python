25f2b41d91870dd8baf21f8bf25e08caed5d62fc
Joshua Ramos <veliciquent@gmail.com>
2021-06-07 15:00:00 -0600

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,16 @@
+
+class Human:
+    arms = 2
+    legs = 2
+    eyes = 2
+    Species = "Homosapien"
+
+class Jeff(Human):
+    gender = "Male"
+    profession = "Dumpster Diver"
+    age = 24
+
+class Jill(Human):
+    glasses = True
+    alcoholic = False
+    IQ = 113
