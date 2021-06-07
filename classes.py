62cdda168ec6ae72ceabe75422993a6e8132041e
Joshua Ramos <veliciquent@gmail.com>
2021-06-07 15:00:48 -0600

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,15 +1,18 @@
 
+#Create parent class "Human"
 class Human:
     arms = 2
     legs = 2
     eyes = 2
     Species = "Homosapien"
 
+#Create child class "Jeff"
 class Jeff(Human):
     gender = "Male"
     profession = "Dumpster Diver"
     age = 24
 
+#Create child class "Jill"
 class Jill(Human):
     glasses = True
     alcoholic = False
