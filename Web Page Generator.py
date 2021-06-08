2a5452c7e11af3166cc4675e2b9e9bde978f85e4
Joshua Ramos <veliciquent@gmail.com>
2021-06-07 22:29:24 -0600

    Update Web Page Generator.py

--- Web Page Generator.py
+++ Web Page Generator.py
@@ -1,12 +1,15 @@
+
+#Importing different modules
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 import tkinter.filedialog
 import webbrowser
 
-
+#Creating class for GUI
 class webGen( Frame ):
     def __init__( self ):
+        #Creating GUI Design including labels, a text input, and a submission button
         tk.Frame.__init__(self)
         self.pack()
         self.master.title("Web Generator")
@@ -23,13 +26,17 @@ class webGen( Frame ):
   
         self.scroll.config(command = self.T1.yview)
         self.scroll.grid(column=4, row=1, rowspan=2,  sticky=N+S+W)
-        
+
+        #Creating the function for the Submission button that creates/overwrites an "index.html" file and then opens it in the computers default browser
     def Generate(self):
+        #Takes the input from the text widget
         Body = input = self.T1.get("1.0",END)
         f = open("index.html", "w")
+        #Overwrites the HTML file altering the "Body" with the text input
         f.write('<!doctype html>\n<html lang="en">\n<head>\n    <meta charset="utf-8">\n    <title>Auto-Generated Web Page</title>\n   <meta name="description" content=">Auto-Generated Web Page">\n\
                 <meta name="Josh" content="SitePoint">\n   </head>\n<body>\n' + Body + '\n</body>\n</html>')
         f.close()
+        #Opens the generated html file in the default browser
         webbrowser.open("index.html", new=0, autoraise=True)
 def main(): 
     webGen().mainloop()
