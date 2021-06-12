842c3095b42cceb6a8dd18fc6a1cfca20aad6f12
Joshua Ramos <veliciquent@gmail.com>
2021-06-12 01:31:30 -0600

    Django Checkbook upload

---
+++ Django_Checkbook/Django_Checkbook/Checkbook/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Account, Transaction
+from .forms import AccountForm, TransactionForm
+
+# Create your views here.
+def home(request):
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        pk = request.POST['account']
+        return balance(request, pk)
+    content = {'form': form}
+    return render(request, 'checkbook/index.html', content)
+
+
+def create_account(request):
+    form = AccountForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'checkbook/CreateNewAccount.html', content)
+
+
+def balance(request, pk):
+    account = get_object_or_404(Account, pk=pk)
+    transactions = Transaction.Transactions.filter(account = pk)
+    current_total = account.initial_deposit
+    table_contents = contents = { }
+    for t in transaction:
+        if t.type == 'Deposit':
+            current_total += t.amount
+            table_contents.update({t: current_total})
+        else:
+            current_total -= t.amount
+            table_contents.update({t: current_total})
+    content = {'account': account, 'table_contents': table_contents, 'balance' : current_total}
+    return render(request, 'checkbook/BalanceSheet.html', content)
+
+
+def transaction(request):
+    form = TransactionForm(data=request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            pk = request.POST['account']
+            form.save()
+            return balance(request, pk)
+    content = {'form': form}
+    return render(request, 'checkbook/AddTransaction.html', content)
