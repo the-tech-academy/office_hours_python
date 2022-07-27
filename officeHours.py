e14dd0c0e6a7d1b0a1e2b6dc7507e24d05c05dfb
catrocky <chrisquattrocchi+1@gmail.com>
2022-07-27 14:58:39 -0700

    Update officeHours.py
    changed hour to hours

--- officeHours.py
+++ officeHours.py
@@ -1,7 +1,6 @@
 title = "Office Hour"
 print(title)
 
-message = "Office hour are from 9:00 am to 9:00 pm."
 print(message)
 
 portlandTime = 8
@@ -88,7 +87,6 @@ def londonBranch():
     elif londontime == 11:
         print("The London branch is closed")
     elif londontimee == 12:
-        print("The London branch is closed")        
     else:
         print("An error has occured")
 
