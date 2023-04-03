90f4e1442b733ccf8730b7f6206a3bd33dd50a3a
GreystoneV <benjaminflevi@gmail.com>
2023-04-03 17:20:41 -0500

    Make changes to hours, occurred and loneontime.

--- officeHours.py
+++ officeHours.py
@@ -1,7 +1,7 @@
 title = "Office Hour"
 print(title)
 
-message = "Office hour are from 9:00 am to 9:00 pm."
+message = "Office hours are from 9:00 am to 9:00 pm."
 print(message)
 
 portlandTime = 8
@@ -34,7 +34,7 @@ def portlandBranch():
     elif portlandTime == 12:
         print("The Portland branch is closed")
     else:
-        print("An error has occured")
+        print("An error has occurred")
 
 def newYorkBranch():
     if newYorkTime == 1:
@@ -87,7 +87,7 @@ def londonBranch():
         print("The London branch is closed")
     elif londontime == 11:
         print("The London branch is closed")
-    elif londontimee == 12:
+    elif londontime == 12:
         print("The London branch is closed")
     else:
         print("An error has occured")
